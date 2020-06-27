cda913254655465e7adbcc1f6efddfee27e34deb
Ayush Bansal <ayushbansal358@gmail.com>
2020-06-27 13:06:16 +0530

    Ready to Deploy

--- assignment1/assignment1-starter-code/app.js
+++ assignment1/assignment1-starter-code/app.js
@@ -1,13 +1,43 @@
 (function(){
 
+  "use strict";
+
   angular.module("LunchCheck",[])
 
   .controller("LunchCheckController",LunchCheckController);
 
-  $scope.text_input = "";
+
   LunchCheckController.$inject = ['$scope'];
-
   function LunchCheckController($scope){
+    $scope.text_input = "";
+    $scope.message = "";
 
+    var count;
+    $scope.countObjects = function(){
+        count = countNum($scope.text_input);
+        $scope.message = displayMessage(count);
+    }
+
+
+    function countNum(string){
+        var res = string.split(",");
+        var count = 0;
+        for(var i=0;i<res.length;i++){
+          if(res[i] !== "")
+            count++;
+        }
+        return count;
+    };
+
+    function displayMessage(count)
+    {
+      var string ;
+      if(count==0){        string = "Please enter data first";}
+      else if(count <=3)  {
+        string ="Enjoy!"; }
+      else{
+        string =" Too Much!";}
+      return string;
+    }
   };
 })();
