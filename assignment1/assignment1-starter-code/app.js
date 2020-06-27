eb0797ee46ca0c73983826b84dcfaafeedf53e82
ayushsbansal <ayushbansal358@gmail.com>
2020-06-27 10:56:16 +0530

    Testing

---
+++ assignment1/assignment1-starter-code/app.js
@@ -0,0 +1,13 @@
+(function(){
+
+  angular.module("LunchCheck",[])
+
+  .controller("LunchCheckController",LunchCheckController);
+
+  $scope.text_input = "";
+  LunchCheckController.$inject = ['$scope'];
+
+  function LunchCheckController($scope){
+    
+  };
+})();
